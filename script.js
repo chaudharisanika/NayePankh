2084a594630e614bbcbfa02b6f5f95af440656c9
Sanika Chaudhari <sanikachaudhari201003@gmail.com>
2023-10-26 00:21:34 +0530

    done

--- script.js
+++ script.js
@@ -1,57 +1,51 @@
-window.addEventListener('scroll', reveall);
+window.addEventListener("scroll", reveall);
 
-function reveall(){
-  var reveals = document.querySelectorAll('.reveall');
+function reveall() {
+  var reveals = document.querySelectorAll(".reveall");
 
-  for(var i = 0; i < reveals.length; i++){
-
+  for (var i = 0; i < reveals.length; i++) {
     var windowheight = window.innerHeight;
     var revealtop = reveals[i].getBoundingClientRect().top;
     var revealpoint = 150;
 
-    if(revealtop < windowheight - revealpoint){
-      reveals[i].classList.add('active');
-    }
-    else{
-      reveals[i].classList.remove('active');
+    if (revealtop < windowheight - revealpoint) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
     }
   }
 }
 
-window.addEventListener('scroll', revealr);
-function revealr(){
-  var reveals = document.querySelectorAll('.revealr');
+window.addEventListener("scroll", revealr);
+function revealr() {
+  var reveals = document.querySelectorAll(".revealr");
 
-  for(var i = 0; i < reveals.length; i++){
-
+  for (var i = 0; i < reveals.length; i++) {
     var windowheight = window.innerHeight;
     var revealtop = reveals[i].getBoundingClientRect().top;
     var revealpoint = 150;
 
-    if(revealtop < windowheight - revealpoint){
-      reveals[i].classList.add('active');
-    }
-    else{
-      reveals[i].classList.remove('active');
+    if (revealtop < windowheight - revealpoint) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
     }
   }
 }
 
-window.addEventListener('scroll', reveal);
-function reveal(){
-  var reveals = document.querySelectorAll('.reveal');
+window.addEventListener("scroll", reveal);
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
 
-  for(var i = 0; i < reveals.length; i++){
-
+  for (var i = 0; i < reveals.length; i++) {
     var windowheight = window.innerHeight;
     var revealtop = reveals[i].getBoundingClientRect().top;
     var revealpoint = 150;
 
-    if(revealtop < windowheight - revealpoint){
-      reveals[i].classList.add('active');
-    }
-    else{
-      reveals[i].classList.remove('active');
+    if (revealtop < windowheight - revealpoint) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
     }
   }
 }
